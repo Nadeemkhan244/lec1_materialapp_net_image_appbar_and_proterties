6b4ccc12650644caaafa3e2870bae3976b9ecb56
Nadeem khan <157910398+Nadeemkhan244@users.noreply.github.com>
2025-09-20 15:13:00 +0500

    lecture1 source code done

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(child: Text(' Nadeem khan')),
+        ),
+        backgroundColor: Colors.lightGreenAccent.shade200,
+        body: Center(
+          child: Image( image: NetworkImage('https://plus.unsplash.com/premium_photo-1733971878574-4d1d01489603?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'))
+      ),
+
+      ),
+  ));
+}
